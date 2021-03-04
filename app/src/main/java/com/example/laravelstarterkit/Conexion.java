417a5a0052a53534a4dfc43bf1455104c4d3e4dd
ulkiobal <ulkiobal@users.noreply.github.com>
2021-03-04 16:32:40 +0100

    Layout modificado

---
+++ app/src/main/java/com/example/laravelstarterkit/Conexion.java
@@ -0,0 +1,6 @@
+package com.example.laravelstarterkit;
+
+public class Conexion {
+    public static String TOKEN = "fNX7tHjS6SjjVNwhJ8eGYkFYh7nSZfVoAZJQdS99";
+    public static String URL_BASE = "http://10.0.2.2:8000/";
+}
--- app/src/main/java/com/example/laravelstarterkit/MainActivity.java
+++ app/src/main/java/com/example/laravelstarterkit/MainActivity.java
@@ -12,24 +12,33 @@ import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txt;
+    private TextView txtTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt = (TextView) findViewById(R.id.textView2);
+        txt = (TextView) findViewById(R.id.txtEstadoApi);
+        txtTitle = (TextView) findViewById(R.id.txtEstadoApiTitle);
         peticionHttp();
     }
 
     private void peticionHttp() {
         AsyncHttpClient client = new AsyncHttpClient();
         client.setMaxRetriesAndTimeout(1, 4000);
-        String url = "http://10.0.2.2:8000/api/test";
-        String token = "fNX7tHjS6SjjVNwhJ8eGYkFYh7nSZfVoAZJQdS99";
+        String url = Conexion.URL_BASE + "api/test";
+        String token = Conexion.TOKEN;
 
         client.addHeader("Authorization", "Bearer " + token);
 
         client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onStart() {
+                txtTitle.setText("¿El token funciona?");
+                txt.setText("Conectando con la API");
+                super.onStart();
+            }
+
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 System.out.println(responseBody);
@@ -39,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                txtTitle.setText("Ha habido un error");
                 String response = new String(error.toString());
                 txt.setText(response);
             }
