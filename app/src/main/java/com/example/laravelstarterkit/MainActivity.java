9e5f8d0bfb9e75f60477154924d6f3824f7e14bb
ulkiobal <ulkiobal@users.noreply.github.com>
2021-03-04 16:19:38 +0100

    Llamada a la api de laravel funcional. IP para comunicar con localhost 10.0.2.2

--- app/src/main/java/com/example/laravelstarterkit/MainActivity.java
+++ app/src/main/java/com/example/laravelstarterkit/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.laravelstarterkit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.textView2);
+        peticionHttp();
+    }
+
+    private void peticionHttp() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setMaxRetriesAndTimeout(1, 4000);
+        String url = "http://10.0.2.2:8000/api/test";
+        String token = "fNX7tHjS6SjjVNwhJ8eGYkFYh7nSZfVoAZJQdS99";
+
+        client.addHeader("Authorization", "Bearer " + token);
+
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                System.out.println(responseBody);
+                String response = new String(responseBody);
+                txt.setText(response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                String response = new String(error.toString());
+                txt.setText(response);
+            }
+        });
+
     }
 }
